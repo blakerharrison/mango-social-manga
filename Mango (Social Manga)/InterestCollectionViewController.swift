c31a506338acd034f561a07e8efe5a89e22bb688
Blake_Harrison <braymondh@gmail.com>
2018-01-25 12:27:26 -0500

    f

---
+++ Mango (Social Manga)/InterestCollectionViewController.swift
@@ -0,0 +1,52 @@
+//
+//  InterestCollectionViewController.swift
+//  Mango (Social Manga)
+//
+//  Created by Blake Harrison on 1/25/18.
+//  Copyright © 2018 Blake Harrison. All rights reserved.
+//
+
+import UIKit
+
+class InterestViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var interest = Interest.fetchInterests()
+    let cellScaling: CGFloat = 0.6
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let screenSize = UIScreen.main.bounds.size
+        let cellWidth = floor(screenSize.width * cellScaling)
+        let cellHeight = floor(screenSize.height * cellScaling)
+        
+        let insetX = (view.bounds.width - cellWidth) / 2.0
+        let insetY = (view.bounds.height - cellHeight) / 2.0
+        
+        let layout = collectionView!.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+        collectionView?.contentInset = UIEdgeInsets(top: insetY, left: insetX, bottom: insetY, right: insetX)
+        
+        collectionView?.dataSource = self
+    }
+}
+
+extension InterestViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return interest.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "InterestCell", for: indexPath) as! MangaCollectionViewCell
+        
+        cell.interest = interest[indexPath.item]
+        
+        return cell
+    }
+    
+}
---
+++ Model/Interest.swift
@@ -0,0 +1,34 @@
+//
+//  Interest.swift
+//  Mango (Social Manga)
+//
+//  Created by Blake Harrison on 1/25/18.
+//  Copyright © 2018 Blake Harrison. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Interest
+{
+    // MARK: - Public API
+    var title = ""
+    var featuredImage: UIImage
+    var color: UIColor
+    
+    init(title: String, featuredImage: UIImage, color: UIColor)
+    {
+        self.title = title
+        self.featuredImage = featuredImage
+        self.color = color
+    }
+    
+    // MARK: - Private
+    // dummy data
+    static func fetchInterests() -> [Interest]
+    {
+        return [
+            Interest(title: "Bleach", featuredImage: UIImage(named: "f1")!, color: UIColor(red: 63/255.0, green: 71/255.0, blue: 80/255.0, alpha: 0.8))
+        ]
+    }
+}
