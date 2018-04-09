7796b3db501f1a363cebb1c096e48a8f46abb0b8
Blake Harrison <braymondh@gmail.com>
2018-04-08 20:04:13 -0400

    Create a new view controller named LandingViewController

--- Mango (Social Manga)/LandingViewController.swift
+++ Mango (Social Manga)/LandingViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import SwiftCarousel
 
 class LandingViewController: UINavigationController {
 
@@ -23,16 +22,14 @@ class LandingViewController: UINavigationController {
     }
     
 
-    let carouselFrame = CGRect(x: view.center.x - 200.0, y: view.center.y - 100.0, width: 400.0, height: 200.0)
-    carouselView = SwiftCarousel(frame: carouselFrame)
-    try! carouselView.itemsFactory(itemsCount: 5) { choice in
-    let imageView = UIImageView(image: UIImage(named: "puppy\(choice+1)"))
-    imageView.frame = CGRect(origin: CGPointZero, size: CGSize(width: 200.0, height: 200.0))
-    
-    return imageView
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
-    carouselView.resizeType = .WithoutResizing(10.0)
-    carouselView.delegate = self
-    carouselView.defaultSelectedIndex = 2
-    view.addSubview(carouselView)
+    */
+
 }
