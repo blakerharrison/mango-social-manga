5b6f078a0577223902467edc7fdf6009f07cfb8c
Blake Harrison <braymondh@gmail.com>
2018-04-08 20:13:35 -0400

    Status bar hidden

--- Mango (Social Manga)/LandingViewController.swift
+++ Mango (Social Manga)/LandingViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class LandingViewController: UINavigationController {
+class LandingViewController: UIViewController{
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,16 +24,5 @@ class LandingViewController: UINavigationController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
