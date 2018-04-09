d99cf632cf3c798a52b5a9eef1700e30aaa36818
Blake Harrison <braymondh@gmail.com>
2018-04-08 22:36:58 -0400

    Added a PageView with additional functionality

--- Mango (Social Manga)/Naruto/NarutoPage.swift
+++ Mango (Social Manga)/Naruto/NarutoPage.swift
@@ -8,21 +8,55 @@
 
 import UIKit
 
-class NarutoPage: UIPageViewController{
-//    , UIPageViewControllerDataSource 
-//    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-//        <#code#>
-//    }
-//
-//    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-//        <#code#>
-//    }
-//
+class NarutoPage: UIPageViewController, UIPageViewControllerDataSource {
 
+    // Sets the Initial Root View
+    lazy var viewControllerList: [UIViewController] = {
+        
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        
+        let vc1 = sb.instantiateViewController(withIdentifier: "Page1")
+        let vc2 = sb.instantiateViewController(withIdentifier: "Page2")
+        let vc3 = sb.instantiateViewController(withIdentifier: "Page3")
+        
+        return [vc1, vc2, vc3]
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.dataSource = self
+        
+        if let firstViewController = viewControllerList.first {
+            self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+            
+        }
+        
     }
-
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+        
+        let previousIndex = vcIndex - 1
+        
+        guard previousIndex >= 0 else {return nil}
+        
+        guard viewControllerList.count > previousIndex else {return nil}
+        
+        return viewControllerList[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let vcIndex =  viewControllerList.index(of: viewController) else {return nil}
+        
+        let nextIndex = vcIndex + 1
+        
+        guard viewControllerList.count != nextIndex else {return nil}
+        
+        guard viewControllerList.count > nextIndex else {return nil}
+        
+        return viewControllerList[nextIndex]
+    }
 }
