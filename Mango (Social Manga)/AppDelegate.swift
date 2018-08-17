f8aa079981557d2b246d9014e1511295ba936cab
Blake Harrison <blakerh@protonmail.com>
2018-08-16 21:54:07 -0400

    Removed sleep timer

--- Mango (Social Manga)/AppDelegate.swift
+++ Mango (Social Manga)/AppDelegate.swift
@@ -13,10 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        Thread.sleep(forTimeInterval: 3.0)
         
         return true
     }
