47a286ace72f164cbd6b86d74ad5b0258aedf19a
Blake Harrison <blakerh@protonmail.com>
2018-08-23 14:43:39 -0400

    Removed code from Home.swift.

--- Mango (Social Manga)/Controller/Home.swift
+++ Mango (Social Manga)/Controller/Home.swift
@@ -9,51 +9,35 @@
 import UIKit
 
 class Home: UIViewController {
-    
-    //MARK: - Properties
-    let userQueue = DispatchQueue.global(qos: .userInitiated)
-    
-    //MARK: - Outlets
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var searchBar: UITextField!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    //MARK: - Methods
-    func setImage() {
-//        print("URL for image: \(imageStringForCover)")
-        
-        guard let url = URL(string: imageStringForCover) else { return }
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if error != nil {
-                print("Failed fetching image:", error!)
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                print("Not a proper HTTPURLResponse or statusCode")
-                
-                let alert = UIAlertController(title: "Connection Error", message: "404", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-                self.present(alert, animated: true)
-                return
-            }
-            
-            DispatchQueue.main.async {
-                self.imageView.image = UIImage(data: data!)
-            }
-            }.resume()
-    }
-    
-    //MARK: - Actions
-    @IBAction func search(_ sender: Any) {
-        
-            MangoNetworking().fetchJSON(search: self.searchBar.text!)
-            self.setImage()
-        }
-        
-    }
+}
 
-
+////MARK: - Methods
+//func setImage() {
+//    //        print("URL for image: \(imageStringForCover)")
+//
+//    guard let url = URL(string: imageStringForCover) else { return }
+//    URLSession.shared.dataTask(with: url) { (data, response, error) in
+//        if error != nil {
+//            print("Failed fetching image:", error!)
+//            return
+//        }
+//
+//        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+//            print("Not a proper HTTPURLResponse or statusCode")
+//
+//            let alert = UIAlertController(title: "Connection Error", message: "404", preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+//            self.present(alert, animated: true)
+//            return
+//        }
+//
+//        DispatchQueue.main.async {
+//            self.imageView.image = UIImage(data: data!)
+//        }
+//        }.resume()
+//}
