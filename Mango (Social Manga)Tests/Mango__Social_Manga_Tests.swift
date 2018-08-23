4a1a9c072f51f95827c2d4ec80e231484c9b5411
Blake Harrison <blakerh@protonmail.com>
2018-08-22 22:42:49 -0400

    Added CI travis yml file.

---
+++ Mango (Social Manga)Tests/Mango__Social_Manga_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Mango__Social_Manga_Tests.swift
+//  Mango (Social Manga)Tests
+//
+//  Created by Blake Harrison on 8/22/18.
+//  Copyright © 2018 Blake Harrison. All rights reserved.
+//
+
+import XCTest
+
+class Mango__Social_Manga_Tests: XCTestCase {
+
+    func testExample() {
+        XCTAssertEqual(5, 5)
+    }
+
+}
