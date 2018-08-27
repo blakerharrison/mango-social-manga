5abbe59704b16f24547291fc6f02f7dd45f0a0b8
Blake Harrison <blakerh@protonmail.com>
2018-08-27 18:53:52 -0400

    Created a viewController.

---
+++ Mango (Social Manga)/Controller/TitleSearch.swift
@@ -0,0 +1,22 @@
+//
+//  TitleSearch.swift
+//  Mango (Social Manga)
+//
+//  Created by Blake Harrison on 8/27/18.
+//  Copyright © 2018 Blake Harrison. All rights reserved.
+//
+
+import UIKit
+
+class TitleSearch: UIViewController {
+
+    //MARK: - Outlets
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
