160aac45ccd9a4388d0968a417f5ef97cbb2e66f
Blake Harrison <blakerh@protonmail.com>
2018-09-09 15:55:02 -0400

    refactored the come controller collection view.

---
+++ Mango (Social Manga)/Controller/MangaReader.swift
@@ -0,0 +1,22 @@
+//
+//  MangaReader.swift
+//  Mango (Social Manga)
+//
+//  Created by Blake Harrison on 9/9/18.
+//  Copyright © 2018 Blake Harrison. All rights reserved.
+//
+
+import UIKit
+
+class MangaReader: UIViewController {
+
+    //MARK: - Outlets
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
