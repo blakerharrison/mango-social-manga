0a98cb630eb2e8a95e144aae8804a76b53b2ae13
Blake Harrison <blakerh@protonmail.com>
2018-10-08 12:03:31 -0400

    Removed bug

--- Mango (Social Manga)Tests/Mango__Social_Manga_Tests.swift
+++ Mango (Social Manga)Tests/Mango__Social_Manga_Tests.swift
@@ -5,17 +5,3 @@
 //  Created by Blake Harrison on 8/22/18.
 //  Copyright © 2018 Blake Harrison. All rights reserved.
 //
-
-import XCTest
-@testable import Mango__Social_Manga_
-
-class Mango__Social_Manga_Tests: XCTestCase {
-    
-    //If manga does not append to usersFavoriteManga the test will fail.
-    func testMangaObjectIsAppendedToUsersFavoriteMangas() {
-        usersFavoriteMangas.append(MangaObject(mangaCover: UIImage(named: "Bleach"), mangaTitles: "Bleach"))
-
-        XCTAssertFalse(usersFavoriteMangas.isEmpty)
-    }
-    
-}
